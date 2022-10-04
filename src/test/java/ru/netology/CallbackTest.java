3dd042ddea330ab224e7d98c67815dbc8d83cf07
k0xzy <koxzy22@gmail.com>
2022-10-04 23:38:27 +0300

    UpdtDZ_selenium

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -48,16 +48,11 @@ class CallbackTest {
     @Test
     void test() {
         driver.get("http://localhost:9999");
-//        List<WebElement> elements = driver.findElements(By.className("input__control"));
-//        elements.get(0).sendKeys("Андрей");
-//        elements.get(1).sendKeys("+79999991111");
-
         driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Андрей");
         driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79999991111");
-
         driver.findElement(By.cssSelector(".checkbox__box")).click();
         driver.findElement(By.cssSelector(".button__text")).click();
-        String text = driver.findElement(By.className("paragraph")).getText();
+        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
